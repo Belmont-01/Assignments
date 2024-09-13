221deab1cdda1ba4c3c559e6e55972a75701cdb4
Belmont-01 <kenshiryan421@gmail.com>
2024-09-13 10:28:47 +0300

    first commit

---
+++ Area of a rectangle.c
@@ -0,0 +1,16 @@
+//Area of a Rectagle
+#include<stdio.h>
+int main(){
+    int a,b,area;//dec and initialization
+    
+     printf("Enter length ");
+     scanf("%d",&a);
+     
+     printf("Enter width ");
+     scanf("%d",&b);
+     
+     area=a*b;
+     printf("The area of a rectangle is %d",area);
+     
+     return 0;
+}
